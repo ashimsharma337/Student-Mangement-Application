a3c91ef1a2d0d418cf1ff1553a66a060d17e89a1
Ashim sharma <ashimsharma337@gmail.com>
2024-03-06 15:05:23 -0500

    Fix: Fix spacing in Student class
    Resolved inconsistent indentation and spacing issues in the Student class. Standardized the use of tabs and spaces for better code readability. No functional changes were made; this commit focuses on code style improvements.

--- StudentMangementApp/src/main/java/com/ashim/bo/Student.java
+++ StudentMangementApp/src/main/java/com/ashim/bo/Student.java
@@ -15,29 +15,29 @@ import lombok.Data;
 @Table(name="student")
 public class Student {
 	
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Long id;
-	
-	@Column(name="first_name")
-	private String firstName;
-	
-	@Column(name="last_name")
-    private String lastName;
-	
-	@Column(name="date_of_birth")
-    private LocalDate dateOfBirth;
-	
-	@Column(name="gender")
-    private String gender;
-	
-	@Column(name="email_address")
-    private String emailAddress;
-	
-	@Column(name="phone_number")
-    private String phoneNumber;
-	
-	@Column(name="address")
-    private String address;
+@Id
+@GeneratedValue(strategy=GenerationType.IDENTITY)
+private Long id;
+
+@Column(name="first_name")
+private String firstName;
+
+@Column(name="last_name")
+private String lastName;
+
+@Column(name="date_of_birth")
+private LocalDate dateOfBirth;
+
+@Column(name="gender")
+private String gender;
+
+@Column(name="email_address")
+private String emailAddress;
+
+@Column(name="phone_number")
+private String phoneNumber;
+
+@Column(name="address")
+private String address;
 
 }
